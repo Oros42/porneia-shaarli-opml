ff773e9b697273a1b67fa7b3f9a0210b1df95d77
Qwerty <champlywood@free.fr>
2014-07-09 20:04:30 +0200

    Update index.php
    Changement de licence.

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-#SOFTWARE IN MIT/X11 LICENCE
-#Copyright ©  2014-07-09, <http://github.com/qwertygc>
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do #so, subject to the following conditions:
-#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#The Software is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders X be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.
-#Except as contained in this notice, the name of the <http://github.com/qwertygc> shall not be used in advertising or otherwise to promote the sale, use or other dealings in this Software without prior written #authorization from the <http://github.com/qwertygc>.
-
+#           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                   Version 2, December 2004
+#Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#Everyone is permitted to copy and distribute verbatim or modified
+#copies of this license document, and changing it is allowed as long
+#as the name is changed.
+#           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
 error_reporting(0);
 //http://porneia.free.fr/pub/links/ou-est-shaarli.html
 if(((fileatime('table.html')+60*60*24*15) < time()) OR !file_exists('table.html')) {
